b7316714e3d530a119a0e68d7e1dbcdc7025ff8d
Andre da silva <andreziobarros@gmail.com>
2019-02-18 21:36:11 -0300

    First Commit

--- Que combustivel usar/ViewController.swift
+++ Que combustivel usar/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stringAlcool: UITextField!
+    @IBOutlet weak var stringGasolina: UITextField!
+    @IBOutlet weak var lblResultString: UILabel!
+    
+    
+    @IBAction func btnCalcMelhorCombustivel(_ sender: Any) {
+        
+        if stringGasolina.text!.isEmpty || stringGasolina.text!.isEmpty{
+            lblResultString.text = "Digite os valores"
+        }else{
+            let strCombustivel = calculeMelhorCombustivel(alcool: stringAlcool.text!, gasolina: stringGasolina.text!)
+            
+            lblResultString.text = strCombustivel
+        }
+        
+    }
+    
+    func calculeMelhorCombustivel(alcool: String, gasolina:String) -> String {
+        let intAlcool = Double(alcool)
+        let intGasolina = Double(gasolina)
+        
+        let divisionCombustivel: Double =  intAlcool! / intGasolina!
+        
+        if divisionCombustivel < 0.7{
+            return "Melhor usar alcool"
+        }
+        
+        return "Melhor usar gasolina"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
